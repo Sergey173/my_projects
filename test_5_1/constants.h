b2d51bbe2b049cec2eaa698c0fccf854d7ff5823
Sergey173 <107751737+Sergey173@users.noreply.github.com>
2022-06-18 14:23:50 +0300

    Add files via upload

---
+++ test_5_1/constants.h
@@ -0,0 +1,8 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+namespace myConstants
+{
+	const double gravityg(9.81);
+
+}
+#endif
---
+++ test_5_1/test_5_1.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <cmath>
+#include "constants.h"
+
+void printHeight(int timePassed, double heightNow)
+{
+	std::cout << "At " << timePassed << " secs ";
+	if (heightNow < 0)
+		std::cout << "The ball is on the ground.\n";
+	else
+		std::cout << "the hight is:" << heightNow << "\n";
+}
+
+void calculateHeight(int height0)
+{
+	double heightNow;
+	int timePassed{ 0 };
+	do
+	{
+		heightNow = height0 - myConstants::gravityg * pow(timePassed, 2) / 2;
+		printHeight(timePassed, heightNow);
+		++timePassed;
+	} while (heightNow > 0);
+}
+
+
+int main()
+{
+	std::cout << "Enter initial height of the ball:\n";
+	int height0;
+	std::cin >> height0;
+
+	calculateHeight(height0);
+
+	std::cin.clear();
+	std::cin.ignore(32767, '\n');
+	std::cin.get();
+
+	return 0;
+}
+
+
